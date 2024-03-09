900ddb9940d09e1077dc0a9a41e4b36433264ff1
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-03-09 22:04:37 +0300

    Added new pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,44 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
-
-const StyledPagination = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 10px;
-  padding-top: 5px;
-`;
-
-let PaginationButton = styled.button`
-  width: 72px;
-  height: 28px;
-  background-color: lawngreen;
-  border: none;
-  border-radius: 10px;
-  cursor: pointer;
-`;
+import ReactPaginate from "react-paginate";
 
 const Pagination = ({ setPage, pageNumber, info, setLoading }) => {
-  let next = () => {
-    if (info === undefined || pageNumber === info.pages) {
-      return;
-    }
-    setLoading(true);
-    setPage((x) => x + 1);
-  };
-  let prev = () => {
-    if (pageNumber === 1) {
-      return;
-    }
-    setLoading(true);
-    setPage((x) => x - 1);
-  };
   return (
-    <StyledPagination>
-      <PaginationButton onClick={prev}>Previous</PaginationButton>
-      <p>{pageNumber}</p>
-      <PaginationButton onClick={next}>Next</PaginationButton>
-    </StyledPagination>
+    <ReactPaginate
+      forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+      className="Pagination"
+      pageCount={info?.pages}
+      breakLabel="..."
+      nextLabel="next >"
+      pageRangeDisplayed={3}
+      pageClassName="paginationPage"
+      previousLabel="< previous"
+      nextClassName="paginateButtonClass"
+      previousClassName="paginateButtonClass"
+      renderOnZeroPageCount={null}
+      onPageChange={(data) => {
+        setLoading(true);
+        setPage(data.selected + 1);
+      }}
+      activeClassName="paginationActive"
+    />
   );
 };
 
